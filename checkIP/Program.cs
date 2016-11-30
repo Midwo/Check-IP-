7a833c4a7fc19c86798e756f88e925b1c250299a
Michał <michal.dwojak92@gmail.com>
2016-11-30 22:10:29 +0100

    Add project files.

---
+++ checkIP/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace checkIP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string NazwaHosta = Dns.GetHostName();
+            IPHostEntry AdresyIP = Dns.GetHostEntry(NazwaHosta);
+            Console.WriteLine("Nazwa komputera: " + NazwaHosta);
+            int licznik = 0;
+            foreach (IPAddress AdresIP in AdresyIP.AddressList)
+            {
+                if (AdresIP.ToString() == "127.0.0.1")
+                {
+                    Console.WriteLine("Komputer nie jest podłączony do sieci. Adres IP to: "
+                        + AdresIP.ToString());
+                }
+                else
+                {
+                    Console.WriteLine("  Adres IP - nr:" + ++licznik + ": " + AdresIP.ToString());
+
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
